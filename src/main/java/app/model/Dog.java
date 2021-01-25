7e1ccf29b1689cf7c9c4aa1fe802a2fd1a495e0b
iyar-work <yar.work@gmail.com>
2021-01-25 15:38:10 +0300

    first commit

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class Dog extends Animal{
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
